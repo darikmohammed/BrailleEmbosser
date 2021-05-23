a73ab5ef613051ddf33cbd6743d34638d17c0f37
darikmohammed <r.m.darik@gmail.com>
2021-05-23 22:25:53 +0300

    uploading document and extracting the docx file

--- Fron-Ang/node_API/app.js
+++ Fron-Ang/node_API/app.js
@@ -1,6 +1,14 @@
 const express = require('express'); 
 const bodyParser = require('body-parser'); 
 const cors = require ('cors');
+const multer = require('multer'); 
+var extract = require('textract'); 
+
+
+//variable inti..
+
+let uploadedFileName; 
+var filePath; 
 
 
 const app = express(); 
@@ -8,10 +16,44 @@ const app = express();
 app.use(express.json());
 app.use(cors()); 
 
+var storage =   multer.diskStorage({
+    destination: function (req, file, callback) {
+      callback(null, './documentUploads');
+    },
+    filename: function (req, file, callback) {
+       uploadedFileName = file.originalname; 
+       callback(null, uploadedFileName);
+    }
+  });
+
+var upload = multer({ storage : storage}).single('userPhoto');
+
 app.post('/text' ,(req,res)=>{
     console.log(req.body); 
 })
 
+app.get('/uploadDocument' , (req,res)=>{
+    res.sendFile(__dirname + "/fileUpload.html");
+})
+app.post('/api/uploadDocument',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        //file uploading sucess!
+        //file name willbe found in variable : uploadedFileName...
+        filePath = './documentUploads/' + uploadedFileName ; 
+        extract.fromFileWithPath(filePath , (error , result) =>{
+            if(error){
+                res.end (error); 
+                return; 
+            }
+            
+            res.end(result); 
+        })
+        //res.end("File is uploaded");
+    });
+});
 
 
 const port = process.env.PORT||3000;
