cca545175801927cae1220036b7bd2906ae69b81
biruk <birukbeletech1212@gmail.com>
2021-05-25 17:03:48 +0300

    voice recorder in angular

--- Fron-Ang/src/app/app.module.ts
+++ Fron-Ang/src/app/app.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { NgAudioRecorderModule } from 'ng-audio-recorder';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { VoiceRecorderComponent } from './voice-recorder/voice-recorder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    VoiceRecorderComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    NgAudioRecorderModule,
     AppRoutingModule,
     ReactiveFormsModule
   ],
